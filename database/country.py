7a1443de3f0d603f42a04e9344a8875d8bed1b71
PyVold <92513901+PyVold@users.noreply.github.com>
2022-02-08 11:56:02 +0100

    Update country.py

--- database/country.py
+++ database/country.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+from flask import jsonify
 import os, requests
 
 dir = os.path.dirname(__file__)
@@ -6,21 +7,38 @@ dir = os.path.dirname(__file__)
 def country_data(country_code):
     datafile = pd.read_csv(os.path.join(dir, 'data.csv'), index_col='Code')
     result = datafile.loc[country_code]
+    #print(result)
     return result.to_dict()
 
 
 def get_country_code(ipaddr):
     try:
-        url = 'https://stat.ripe.net/data/whois/data.json?resource={}'.format(ipaddr)
-        response = requests.get(url).json()['data']['records'][0]
+        #url = 'https://stat.ripe.net/data/whois/data.json?resource={}'.format(ipaddr)
+        url = 'https://stat.ripe.net/data/maxmind-geo-lite/data.json?resource={}'.format(ipaddr)
+        #response = requests.get(url).json()['data']['records']
+        response = requests.get(url).json()['data']['located_resources'][0]['locations']
+        #print(response)
+        #len(response)
+        #return response
     except:
         country_code = 'private'
         return country_code
+
     try:
-        for dct in response:
-            if dct['key'] == 'country':
-                country_code = dct['value']
-        return country_code
+        for el in response:
+            country_code = el['country']
+            '''
+            for dct in el:
+                if dct['key'].lower() == 'country':
+                    country_code = dct['value']
+                    #print(country_code)
+                    return country_code
+
+                else:
+                    country_code = 'private'
+            '''
+        return country_code            
+        
     except:
         country_code = 'private'
         return country_code
